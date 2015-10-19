f833285a67d5b8e6cc56750c48e3ec561ad46461
Josha von Gizycki <josha.gizycki@triology.de>
2015-10-19 13:41:56 +0200

    added tests and record set

---
+++ src/laniger/PhpCharts/model/RecordSet.php
@@ -0,0 +1,60 @@
+<?php
+namespace laniger\PhpCharts\model;
+
+/**
+ * set of data appliable to a diagram
+ *
+ * @author jgizycki
+ */
+class RecordSet
+{
+
+    /**
+     * identificator for this record set
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * array of values
+     * @var array
+     */
+    private $values;
+
+    /**
+     * ctor
+     * @param string $name series identificator
+     */
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * adds a value to this series
+     * @param numeric $value
+     * @return RecordSet
+     */
+    public function add($value)
+    {
+        $this->values[] = $value;
+        return $this;
+    }
+
+    /**
+     * @return string the series identificator
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return \Iterator
+     */
+    public function getValues()
+    {
+        return new \ArrayIterator($this->values);
+    }
+}
---
+++ tests/laniger/PhpCharts/model/RecordSetTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace laniger\PhpCharts\model;
+
+class RecordSetTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     *
+     * @return \laniger\PhpCharts\model\RecordSet
+     */
+    public function provide()
+    {
+        return new RecordSet('series');
+    }
+
+
+    /**
+     * @depends provide
+     * @covers laniger\PhpCharts\model\RecordSet::getSeries()
+     *
+     * @param RecordSet $set
+     */
+    public function testGetName(RecordSet $set)
+    {
+        $this->assertEquals('series', $set->getName());
+    }
+
+    /**
+     * @depends provide
+     *
+     * @param RecordSet $set
+     */
+    public function testAdd(RecordSet $set)
+    {
+        $this->assertInstanceOf(RecordSet::class, $set->add(10));
+        return $set;
+    }
+
+    /**
+     * @depends testAdd
+     *
+     * @param RecordSet $set
+     */
+    public function testGetValues(RecordSet $set)
+    {
+        $this->assertInstanceOf(\Iterator::class, $set->getValues());
+
+        foreach($set->getValues() as $value) {
+            $this->assertEquals(10, $value);
+        }
+    }
+
+}
